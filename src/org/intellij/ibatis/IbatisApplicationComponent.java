fa45af7ea6cfb81833f11f6d849647be68431e7e
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-18 14:02:30 +0000

    file template for sql map xml file added
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@10 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/IbatisApplicationComponent.java
+++ src/org/intellij/ibatis/IbatisApplicationComponent.java
@@ -3,10 +3,14 @@ package org.intellij.ibatis;
 import com.intellij.codeInspection.InspectionToolProvider;
 import com.intellij.facet.FacetTypeRegistry;
 import com.intellij.ide.IconProvider;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.ide.fileTemplates.impl.FileTemplateImpl;
+import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.javaee.ExternalResourceManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.psi.PsiElement;
 import org.intellij.ibatis.facet.IbatisFacetType;
+import org.intellij.ibatis.insepections.SqlMapFileInConfigurationInspection;
 import org.intellij.ibatis.insepections.SqlMapUniqueIdInspection;
 import org.intellij.ibatis.util.IbatisConstants;
 import org.jetbrains.annotations.NonNls;
@@ -21,6 +25,10 @@ import javax.swing.*;
  * @authtor Jacky
  */
 public class IbatisApplicationComponent implements ApplicationComponent, InspectionToolProvider, IconProvider {
+    private static String SQLMAP_CONTENT = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\n" +
+            "<!DOCTYPE sqlMap PUBLIC \"-//ibatis.apache.org//DTD SQL Map 2.0//EN\"\n" +
+            "    \"http://ibatis.apache.org/dtd/sql-map-2.dtd\">\n\n" +
+            "<sqlMap>\n\n</sqlMap>";
 
     public IbatisApplicationComponent() {
     }
@@ -29,6 +37,12 @@ public class IbatisApplicationComponent implements ApplicationComponent, Inspect
         registerDTDs(IbatisConstants.CONFIGURATION_DTDS);
         registerDTDs(IbatisConstants.SQLMAP_DTDS);
         FacetTypeRegistry.getInstance().registerFacetType(IbatisFacetType.INSTANCE);
+        final FileTemplateManager fileTemplateManager = FileTemplateManager.getInstance();
+        if (fileTemplateManager.getInternalTemplate("iBATIS Sql Map File") == null) {
+            FileTemplateImpl template = (FileTemplateImpl) fileTemplateManager.addTemplate("iBATIS Sql Map File", XmlFileType.DEFAULT_EXTENSION);
+            template.setInternal(true);
+            template.setText(SQLMAP_CONTENT);
+        }
     }
 
     public void disposeComponent() {
@@ -70,6 +84,6 @@ public class IbatisApplicationComponent implements ApplicationComponent, Inspect
      * @return inspection class array
      */
     public Class[] getInspectionClasses() {
-        return new Class[]{SqlMapUniqueIdInspection.class};
+        return new Class[]{SqlMapUniqueIdInspection.class, SqlMapFileInConfigurationInspection.class};
     }
 }
