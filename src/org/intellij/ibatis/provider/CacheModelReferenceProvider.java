98e965d2796538864dbdb31a1d49cc33b84c1b7a
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-20 06:20:57 +0000

    cache model reference provider
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@20 a27988d2-9830-0410-8773-31d6671a21de

---
+++ src/org/intellij/ibatis/provider/CacheModelReferenceProvider.java
@@ -0,0 +1,40 @@
+package org.intellij.ibatis.provider;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.xml.XmlAttributeValue;
+import org.intellij.ibatis.IbatisManager;
+import org.intellij.ibatis.dom.sqlMap.CacheModel;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * cache model reference provider
+ */
+public class CacheModelReferenceProvider extends BaseReferenceProvider {
+    @NotNull public PsiReference[] getReferencesByElement(PsiElement psiElement) {
+        XmlAttributeValue xmlAttributeValue = (XmlAttributeValue) psiElement;
+        XmlAttributeValuePsiReference psiReference = new XmlAttributeValuePsiReference(xmlAttributeValue) {
+            public boolean isSoft() {
+                return false;
+            }
+
+            @Nullable public PsiElement resolve() {
+                String cacheModelId = getCanonicalText();
+                Map<String, CacheModel> allResultMap = IbatisManager.getInstance().getAllCacheModel(getElement());
+                CacheModel cacheModel = allResultMap.get(cacheModelId);
+                return cacheModel == null ? null : cacheModel.getId().getXmlAttribute();
+            }
+
+            public Object[] getVariants() {
+                Set<String> cacheModelList = IbatisManager.getInstance().getAllCacheModel(getElement()).keySet();
+                return cacheModelList.toArray();
+            }
+        };
+        return new PsiReference[]{psiReference};
+    }
+
+}
