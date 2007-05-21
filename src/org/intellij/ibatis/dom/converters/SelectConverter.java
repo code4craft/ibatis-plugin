97bb9557d6a9c86a89380b73b3031d79b8f82074
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-21 04:05:01 +0000

    icon added for variants
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@37 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/dom/converters/SelectConverter.java
+++ src/org/intellij/ibatis/dom/converters/SelectConverter.java
@@ -1,5 +1,6 @@
 package org.intellij.ibatis.dom.converters;
 
+import com.intellij.codeInsight.lookup.LookupValueFactory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.xml.XmlAttributeValue;
@@ -10,6 +11,7 @@ import com.intellij.util.xml.GenericDomValue;
 import org.intellij.ibatis.IbatisManager;
 import org.intellij.ibatis.dom.sqlMap.Select;
 import org.intellij.ibatis.provider.XmlAttributeValuePsiReference;
+import org.intellij.ibatis.util.IbatisConstants;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * select statement converter
@@ -39,22 +42,19 @@ public class SelectConverter extends Converter<Select> implements CustomReferenc
             }
 
             public Object[] getVariants() {
-                Map<String, Select> allSelect = IbatisManager.getInstance().getAllSelect(getElement());
-                List<String> allSelectNames = new ArrayList<String>();
-                for (Select select : allSelect.values()) {
-                    allSelectNames.add(select.getId().getValue());
+                List<Object> variants = new ArrayList<Object>();
+                Set<String> selectList = IbatisManager.getInstance().getAllSelect(getElement()).keySet();
+                for (String select : selectList) {
+                    variants.add(LookupValueFactory.createLookupValue(select, IbatisConstants.SQLMAP_SELECT));
                 }
-                return allSelectNames.toArray();
+                return variants.toArray();
             }
 
             @Nullable public PsiElement resolve() {
-                String selectId = getCanonicalText();
-                Map<String, Select> allSelect = IbatisManager.getInstance().getAllSelect(getElement());
-                for (Select select : allSelect.values()) {
-                    if (selectId.equals(select.getId().getValue()))
-                        return select.getXmlTag();
-                }
-                return null;
+                String resultMapId = getCanonicalText();
+                Map<String, Select> allResultMap = IbatisManager.getInstance().getAllSelect(getElement());
+                Select select = allResultMap.get(resultMapId);
+                return select == null ? null : select.getId().getXmlAttribute();
             }
         }};
     }
