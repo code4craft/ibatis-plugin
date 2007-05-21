eb6bc16bb27ade48a03c1164da2fb785658380dc
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-21 02:46:46 +0000

    more static icon variable added
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@32 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/util/IbatisConstants.java
+++ src/org/intellij/ibatis/util/IbatisConstants.java
@@ -14,7 +14,21 @@ public interface IbatisConstants {
     };
 
     public Icon IBATIS_LOGO = IconLoader.findIcon("/icons/logo.png");
+    
+    //class related
+    public Icon CLASS_FIELD = IconLoader.findIcon("/icons/class_field.png");
+    public Icon INTERNAL_CLASS = IconLoader.findIcon("/icons/internal_class.png");
+    public Icon TYPE_ALIAS = IconLoader.findIcon("/icons/typealias.png");
+    
+    //database related field
     public Icon DATABASE_PK_FIELD = IconLoader.findIcon("/icons/dataPkColumn.png");
     public Icon DATABASE_COMMON_FIELD = IconLoader.findIcon("/icons/dataColumn.png");
-    public Icon CLASS_FIELD = IconLoader.findIcon("/icons/class_field.png");
+
+    //icon for sentence in sql map xml file
+    public Icon SQLMAP_SELECT = IconLoader.findIcon("/icons/sqlmap/select.png");
+    public Icon SQLMAP_DELETE = IconLoader.findIcon("/icons/sqlmap/delete.png");
+    public Icon SQLMAP_INSERT = IconLoader.findIcon("/icons/sqlmap/insert.png");
+    public Icon SQLMAP_UPDATE = IconLoader.findIcon("/icons/sqlmap/update.png");
+    public Icon SQLMAP_PROCEDURE = IconLoader.findIcon("/icons/sqlmap/procedure.png");
+    public Icon SQLMAP_STATEMENT = IconLoader.findIcon("/icons/sqlmap/statement.png");
 }
