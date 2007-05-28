86f6661aa8a1bd6035a7ea5c3e9900d6f447686c
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-28 11:30:28 +0000

    inpsection for set null to primary type variable
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@61 a27988d2-9830-0410-8773-31d6671a21de

---
+++ src/org/intellij/ibatis/insepections/NullSettedToPrimaryTypeInspection.java
@@ -0,0 +1,98 @@
+package org.intellij.ibatis.insepections;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiType;
+import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.xml.highlighting.DomElementAnnotationHolder;
+import org.intellij.ibatis.IbatisSqlMapModel;
+import org.intellij.ibatis.dom.sqlMap.Result;
+import org.intellij.ibatis.dom.sqlMap.ResultMap;
+import org.intellij.ibatis.dom.sqlMap.SqlMap;
+import org.intellij.ibatis.provider.FieldAccessMethodReferenceProvider;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * inpsection for set null to primary type variable
+ */
+public class NullSettedToPrimaryTypeInspection extends SqlMapInspection {
+    @Nls @NotNull public String getDisplayName() {
+        return "set null to primary type variable";
+    }
+
+    @NonNls @NotNull public String getShortName() {
+        return "NullToPrimaryTypeVariable";
+    }
+
+    @SuppressWarnings({"ConstantConditions"})
+    protected void checkResultMap(IbatisSqlMapModel sqlMapModel, SqlMap sqlMap, ResultMap resultMap, DomElementAnnotationHolder holder) {
+        PsiClass psiClass = resultMap.getPsiClass();
+        if (psiClass == null) return;
+        List<Result> results = resultMap.getResults();
+        for (Result result : results) {
+            if (result.getXmlTag().getAttribute("nullValue") == null) {    //null value setted
+                String propertyName = result.getProperty().getValue();
+                PsiMethod setMethod = null;
+                if (!propertyName.contains(".")) {
+                    PsiMethod[] methods = psiClass.findMethodsByName("set" + StringUtil.capitalize(propertyName), true);
+                    if (methods.length > 0) {
+                        setMethod = methods[0];
+                    }
+                } else      //deep child
+                {
+                    String field1 = propertyName.substring(0, propertyName.indexOf('.'));
+                    String field2 = propertyName.substring(propertyName.indexOf('.') + 1);
+                    PsiClass fieldClass = FieldAccessMethodReferenceProvider.findGetterMethodReturnType(psiClass, "get" + StringUtil.capitalize(field1));
+                    if (fieldClass != null) {
+                        PsiMethod[] methods = fieldClass.findMethodsByName("set" + StringUtil.capitalize(field2), true);
+                        if (methods.length > 0) {
+                            setMethod = methods[0];
+                        }
+                    }
+                }
+                if (setMethod != null) {
+                    PsiType[] superTypes = setMethod.getReturnType().getSuperTypes();
+                    if (superTypes.length < 1)  // primary type
+                    {
+                        holder.createProblem(result, HighlightSeverity.WARNING, "Null value may be setted to primary type variable.", new AddNullValueForResultElementQuickFix(result));
+                    }
+                }
+            }
+        }
+    }
+
+    public class AddNullValueForResultElementQuickFix implements LocalQuickFix {
+        private Result result;
+
+        public AddNullValueForResultElementQuickFix(Result result) {
+
+            this.result = result;
+        }
+
+        @NotNull public String getName() {
+            return "add nullValue for result element";
+        }
+
+        @NotNull public String getFamilyName() {
+            return "add nullValue for result element";
+        }
+
+        @SuppressWarnings({"ConstantConditions"})
+        public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
+            try {
+                result.getXmlTag().setAttribute("nullValue", "0");
+            } catch (IncorrectOperationException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
