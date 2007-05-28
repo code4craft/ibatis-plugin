2c8c7eb17f3900c53b15e5c16b9aa5666adc106d
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-28 11:46:30 +0000

    getReferencedResultMap implemented
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@66 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/dom/sqlMap/impl/SelectImpl.java
+++ src/org/intellij/ibatis/dom/sqlMap/impl/SelectImpl.java
@@ -1,11 +1,26 @@
 package org.intellij.ibatis.dom.sqlMap.impl;
 
-import org.intellij.ibatis.dom.sqlMap.Select;
-import com.intellij.javaee.model.xml.impl.RootBaseImpl;
 import com.intellij.javaee.model.xml.impl.BaseImpl;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.xml.DomElement;
+import org.intellij.ibatis.IbatisManager;
+import org.intellij.ibatis.dom.sqlMap.ResultMap;
+import org.intellij.ibatis.dom.sqlMap.Select;
 
 /**
  * select element implementatioin.
  */
 public abstract class SelectImpl extends BaseImpl implements Select {
+    public ResultMap getReferencedResultMap() {
+        String resultMapName = getResultMap().getValue();
+        if (StringUtil.isNotEmpty(resultMapName)) {
+            XmlTag tag = IbatisManager.getInstance().getAllParameterMap2(getXmlElement()).get(resultMapName);
+            DomElement element = getManager().getDomElement(tag);
+            if (element instanceof ResultMap) {
+                return (ResultMap) element;
+            }
+        }
+        return null;
+    }
 }
