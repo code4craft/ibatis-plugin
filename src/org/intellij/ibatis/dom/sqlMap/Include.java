4c898d23cc4f52a33e7705d8a316846edc75ec05
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-28 12:55:47 +0000

    getValue added
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@73 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/dom/sqlMap/Include.java
+++ src/org/intellij/ibatis/dom/sqlMap/Include.java
@@ -2,11 +2,13 @@ package org.intellij.ibatis.dom.sqlMap;
 
 import com.intellij.javaee.model.xml.CommonDomModelElement;
 import com.intellij.util.xml.GenericAttributeValue;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.util.xml.TagValue;
 
 /**
  * sql select element
  */
 public interface Include extends CommonDomModelElement {
     public GenericAttributeValue<String> getRefid();
+
+    @TagValue String getValue();
 }
