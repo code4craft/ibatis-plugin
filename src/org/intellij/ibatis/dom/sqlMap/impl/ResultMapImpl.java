c1fc1a5ff3cc6c963fd156c437a0701f7a9e6137
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-05-28 13:05:23 +0000

    getAllResults method implemented
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@78 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/dom/sqlMap/impl/ResultMapImpl.java
+++ src/org/intellij/ibatis/dom/sqlMap/impl/ResultMapImpl.java
@@ -3,8 +3,14 @@ package org.intellij.ibatis.dom.sqlMap.impl;
 import com.intellij.javaee.model.xml.impl.BaseImpl;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.xml.DomElement;
+import org.intellij.ibatis.IbatisManager;
+import org.intellij.ibatis.dom.sqlMap.Result;
 import org.intellij.ibatis.dom.sqlMap.ResultMap;
 import org.intellij.ibatis.provider.IbatisClassShortcutsReferenceProvider;
+
+import java.util.List;
 
 /**
  * resultMap element implementation
@@ -13,8 +19,24 @@ public abstract class ResultMapImpl extends BaseImpl implements ResultMap {
     public PsiClass getPsiClass() {
         String classname = getClazz().getValue();
         if (StringUtil.isNotEmpty(classname)) {
-          return  IbatisClassShortcutsReferenceProvider.getPsiClass(getClazz().getXmlAttribute(), classname);
+            return IbatisClassShortcutsReferenceProvider.getPsiClass(getClazz().getXmlAttribute(), classname);
         }
         return null;
+    }
+
+    public List<Result> getAllResults() {
+        List<Result> results = getResults();
+        String extendedResultMapName = getExtends().getValue();
+        if (StringUtil.isNotEmpty(extendedResultMapName)) {
+            XmlTag tag = IbatisManager.getInstance().getAllResultMap2(getXmlElement()).get(extendedResultMapName);
+            if (tag != null) {
+                DomElement element = getManager().getDomElement(tag);
+                if (element != null && element instanceof ResultMap) {
+                    ResultMap parentResultMap = (ResultMap) element;
+                    results.addAll(parentResultMap.getResults());
+                }
+            }
+        }
+        return results;
     }
 }
