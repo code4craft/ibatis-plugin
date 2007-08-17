5e2552f3ca5a3814713c86a7f62b7a486b111457
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-08-17 11:44:08 +0000

    add two methods
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@133 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/provider/TableColumnReferenceProvider.java
+++ src/org/intellij/ibatis/provider/TableColumnReferenceProvider.java
@@ -7,13 +7,16 @@ import com.intellij.javaee.dataSource.DatabaseTableFieldData;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.javadoc.PsiDocTag;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlTag;
 import org.intellij.ibatis.util.IbatisConstants;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,32 +31,64 @@ public class TableColumnReferenceProvider extends BaseReferenceProvider {
         if (xmlTag.getAttributeValue("class") != null) {
             final PsiClass psiClass = IbatisClassShortcutsReferenceProvider.getPsiClass(xmlAttributeValue, xmlTag.getAttributeValue("class"));
             if (psiClass != null) {
-                PsiDocComment docComment = psiClass.getDocComment();
-                if (docComment != null) {
-                    PsiDocTag tableTag = docComment.findTagByName("table");
-                    if (tableTag != null) {
-                        String tableName = tableTag.getValueElement().getText().trim();
-                        if (StringUtil.isNotEmpty(tableName)) {
-                            DataSource dataSource = DatabaseTableReferenceProvider.getDataSourceForIbatis(psiElement);
-                            if (dataSource != null) {
-                                DatabaseTableData tableData = null;
-                                List<DatabaseTableData> tables = dataSource.getTables();
-                                for (DatabaseTableData table : tables) {
-                                    if (table.getName().equals(tableName)) {                                        
-                                        tableData = table;
-                                        break;
-                                    }
-                                }
-                                if (tableData != null) {
-                                    return new PsiReference[]{new TableColumnReference(xmlAttributeValue, tableData)};
-                                }
-                            }
+                DatabaseTableData tableData = getDatabaseTableData(psiClass);
+                if (tableData != null) {
+                    return new PsiReference[]{new TableColumnReference(xmlAttributeValue, tableData)};
+                }
+            }
+        }
+        return PsiReference.EMPTY_ARRAY;
+    }
+
+    /**
+     * get tableData in  PsiClass
+     *
+     * @param psiClass PsiClass
+     * @return tableData object
+     */
+    @Nullable
+    public static DatabaseTableData getDatabaseTableData(PsiClass psiClass) {
+        PsiDocComment docComment = psiClass.getDocComment();
+        if (docComment != null && docComment.findTagByName("table") != null) {
+            PsiDocTag tableTag = docComment.findTagByName("table");
+            String tableName = tableTag.getValueElement().getText().trim();
+            if (StringUtil.isNotEmpty(tableName)) {
+                DataSource dataSource = JavadocTableNameReferenceProvider.getDataSourceForIbatis(psiClass);
+                if (dataSource != null) {
+                    List<DatabaseTableData> tables = dataSource.getTables();
+                    for (DatabaseTableData table : tables) {
+                        if (table.getName().equals(tableName)) {
+                            return table;
                         }
                     }
                 }
             }
         }
-        return PsiReference.EMPTY_ARRAY;
+        return null;
+    }
+
+    /**
+     * get tableFieldData in psiMethod
+     *
+     * @param psiMethod PsiMethod object
+     * @return DatabaseTableFieldData object
+     */
+    @Nullable
+    public static DatabaseTableFieldData getDatabaseTableFieldData(PsiMethod psiMethod) {
+        PsiDocComment docComment = psiMethod.getDocComment();
+        if (docComment != null && docComment.findTagByName("column") != null) {
+            PsiDocTag psiDocTag = docComment.findTagByName("column");
+            String columnName = psiDocTag.getValueElement().getText().trim();
+            if (StringUtil.isNotEmpty(columnName)) {
+                PsiClass psiClass = PsiTreeUtil.getParentOfType(psiMethod, PsiClass.class);
+                if (psiClass != null) {
+                    DatabaseTableData tableData = getDatabaseTableData(psiClass);
+                    if (tableData != null)
+                        return tableData.findColumnByName(columnName);
+                }
+            }
+        }
+        return null;
     }
 
     public class TableColumnReference extends XmlAttributeValuePsiReference {
