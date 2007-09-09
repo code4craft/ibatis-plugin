b24c90f907086ce90b9662ad10f02f1a55927cf1
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-09-09 07:02:51 +0000

    minor update
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@182 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/provider/SelectorSymbolCompletionData.java
+++ src/org/intellij/ibatis/provider/SelectorSymbolCompletionData.java
@@ -6,23 +6,25 @@ import com.intellij.codeInsight.completion.CompletionVariant;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReference;
 import com.intellij.psi.filters.TrueFilter;
 import com.intellij.psi.filters.position.LeftNeighbour;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlText;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomFileElement;
 import com.intellij.util.xml.DomManager;
-import org.intellij.ibatis.dom.sqlMap.ResultMap;
+import com.intellij.javaee.dataSource.DataSource;
+import com.intellij.javaee.dataSource.DatabaseTableData;
+import com.intellij.javaee.dataSource.DatabaseTableFieldData;
 import org.intellij.ibatis.dom.sqlMap.SqlMap;
-import org.intellij.ibatis.dom.sqlMap.Result;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * completion data for selector symbol
@@ -46,24 +48,26 @@ public class SelectorSymbolCompletionData extends CompletionData {
     }
 
     public CompletionVariant[] findVariants(PsiElement psiElement, CompletionContext completionContext) {
-        PsiFile psiFile = completionContext.file;
-        String prefix = completionContext.getPrefix();
-        if (psiFile instanceof XmlFile) {   //xml file and prefix match
-            final DomFileElement fileElement = DomManager.getDomManager(completionContext.project).getFileElement((XmlFile) completionContext.file, DomElement.class);
-            if (fileElement != null && fileElement.getRootElement() instanceof SqlMap) {
-                XmlTag tag = getParentSentence(psiElement);
-                if (tag != null && !prefix.contains("#")) {   //not symbol
-                    LeftNeighbour left = new LeftNeighbour(TrueFilter.INSTANCE);
-                    CompletionVariant variant = new CompletionVariant(left);
-                    List<String> parameterNames = getSelectorSymbolsForXmlTag(tag);
-                    if (parameterNames.size() > 0) {
-                        for (String parameterName : parameterNames) {
-                            variant.addCompletion(parameterName);
+        if (psiElement.getParent() instanceof XmlText) {   // text only
+            PsiFile psiFile = completionContext.file;
+            String prefix = completionContext.getPrefix();
+            if (psiFile instanceof XmlFile) {   //xml file and prefix match
+                final DomFileElement fileElement = DomManager.getDomManager(completionContext.project).getFileElement((XmlFile) completionContext.file, DomElement.class);
+                if (fileElement != null && fileElement.getRootElement() instanceof SqlMap) {
+                    XmlTag tag = SqlMapSymbolCompletionData.getParentSentence(psiElement);
+                    if (tag != null && !prefix.contains("#")) {   //not symbol
+                        LeftNeighbour left = new LeftNeighbour(TrueFilter.INSTANCE);
+                        CompletionVariant variant = new CompletionVariant(left);
+                        List<String> parameterNames = getSelectorSymbolsForXmlTag(tag);
+                        if (parameterNames.size() > 0) {
+                            for (String parameterName : parameterNames) {
+                                variant.addCompletion(parameterName);
+                            }
+                            variant.includeScopeClass(PsiElement.class, true);
+                            variant.addCompletionFilter(TrueFilter.INSTANCE);
+                            variant.setInsertHandler(new SqlMapSymbolnsertHandler());
+                            return new CompletionVariant[]{variant};
                         }
-                        variant.includeScopeClass(PsiElement.class, true);
-                        variant.addCompletionFilter(TrueFilter.INSTANCE);
-                        variant.setInsertHandler(new SqlMapSymbolnsertHandler());
-                        return new CompletionVariant[]{variant};
                     }
                 }
             }
@@ -80,22 +84,78 @@ public class SelectorSymbolCompletionData extends CompletionData {
      */
     public List<String> getSelectorSymbolsForXmlTag(XmlTag xmlTag) {
         List<String> nameList = new ArrayList<String>();
-        XmlAttribute attribute = xmlTag.getAttribute("resultMap");
-        if (attribute != null) {
-            PsiReference psiReference = attribute.getValueElement().getReference();
-            if (psiReference != null) {
-                PsiElement psiElement = psiReference.resolve();
-                if (psiElement != null && psiElement instanceof XmlAttribute) {  //resultMap element
-                    ResultMap resultMap = (ResultMap) DomManager.getDomManager(xmlTag.getProject()).getDomElement(PsiTreeUtil.getParentOfType(psiElement, XmlTag.class));
-                    if (resultMap != null) {
-                        for (Result result : resultMap.getAllResults()) {
-                            nameList.add(result.getColumn().getValue());
-                        }
+        String tableName = getTableName(xmlTag);
+        if (tableName != null) {
+            DataSource dataSource = JavadocTableNameReferenceProvider.getDataSourceForIbatis(xmlTag);
+            if (dataSource != null) {
+                DatabaseTableData table = null;
+                List<DatabaseTableData> tableList = dataSource.getTables();
+                for (DatabaseTableData databaseTableData : tableList) {
+                    if (databaseTableData.getName().equalsIgnoreCase(tableName)) {  //table name match
+                        table = databaseTableData;
+                        break;
+                    }
+                }
+                if (table != null) {
+                    List<DatabaseTableFieldData> fieldList = table.getFields();
+                    for (DatabaseTableFieldData databaseTableFieldData : fieldList) {
+                        nameList.add(databaseTableFieldData.getName());
+
                     }
                 }
             }
         }
         return nameList;
+    }
+
+    @Nullable
+    public String getTableName(XmlTag xmlTag) {
+        String sql = getSQL(xmlTag).trim().toLowerCase();
+        if (sql.startsWith("insert")) {   // insert
+            String tableName = sql.substring(sql.indexOf("into") + 4).trim();
+            tableName = tableName.substring(0, tableName.indexOf(" "));  //todo table name parser
+            return tableName;
+        } else if (sql.startsWith("update")) {     //update
+
+        } else {  //select and delete
+            String[] parts = sql.split("from \\s*");
+            if (parts.length > 1) {
+                String tableName = parts[1].substring(0, parts[1].indexOf(" ")).trim();
+                if (tableName.contains("."))
+                    tableName = tableName.substring(0, tableName.indexOf("."));
+                return tableName;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * got SQL code in sentence
+     *
+     * @param xmlTag xml tag
+     * @return SQL code in sentence
+     */
+    @SuppressWarnings({"StringConcatenationInsideStringBufferAppend"}) public String getSQL(XmlTag xmlTag) {
+        StringBuilder sql = new StringBuilder();
+        PsiElement[] children = xmlTag.getChildren();
+        for (PsiElement child : children) {
+            if (child instanceof XmlTag) {
+                XmlTag tag = (XmlTag) child;
+                if (tag.getName().equals("include")) {
+                    XmlAttribute refid = tag.getAttribute("refid");
+                    if (refid != null && StringUtil.isNotEmpty(refid.getText())) {
+                        PsiElement psiElement = refid.getValueElement().getReference().resolve();
+                        if (psiElement instanceof XmlAttribute) {
+                            XmlAttribute idAttribute = (XmlAttribute) psiElement;
+                            sql.append(" " + idAttribute.getParent().getValue().getText());
+                        }
+                    }
+                }
+            } else if (child instanceof XmlText) {
+                sql.append(" " + ((XmlText) child).getValue());
+            }
+        }
+        return sql.toString();
     }
 
 
