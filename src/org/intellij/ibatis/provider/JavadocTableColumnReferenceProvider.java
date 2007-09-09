a11dc0380d72bf608524e96133d3c90557a0d231
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2007-09-09 08:43:28 +0000

    minor update
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@186 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/provider/JavadocTableColumnReferenceProvider.java
+++ src/org/intellij/ibatis/provider/JavadocTableColumnReferenceProvider.java
@@ -64,9 +64,9 @@ public class JavadocTableColumnReferenceProvider extends BaseReferenceProvider {
                     if (tableData != null) {
                         for (DatabaseTableFieldData field : tableData.getFields()) {
                             if (field.isPrimary()) {       //pk
-                                variants.add(LookupValueFactory.createLookupValueWithHint(field.getName(), IbatisConstants.DATABASE_PK_FIELD, field.getType()));
+                                variants.add(LookupValueFactory.createLookupValueWithHint(field.getName(), IbatisConstants.DATABASE_PK_FIELD, TableColumnReferenceProvider.getJdbcTypeName(field.getJdbcType())));
                             } else {   //common column
-                                variants.add(LookupValueFactory.createLookupValueWithHint(field.getName(),IbatisConstants.DATABASE_COMMON_FIELD,field.getType()));
+                                variants.add(LookupValueFactory.createLookupValueWithHint(field.getName(), IbatisConstants.DATABASE_COMMON_FIELD, TableColumnReferenceProvider.getJdbcTypeName(field.getJdbcType())));
                             }
                         }
                     }
