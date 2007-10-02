0b1c1f5976ad2eceae4045df004e229091adae10
larry.meadors <larry.meadors@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-02 04:12:15 +0000

    Starting to build parameter map support
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@217 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/intention/GenerateSQLForInsertAction.java
+++ src/org/intellij/ibatis/intention/GenerateSQLForInsertAction.java
@@ -1,37 +1,37 @@
 package org.intellij.ibatis.intention;
 
+import com.intellij.javaee.dataSource.DatabaseTableData;
+import com.intellij.javaee.dataSource.DatabaseTableFieldData;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomManager;
-import com.intellij.javaee.dataSource.DatabaseTableData;
-import com.intellij.javaee.dataSource.DatabaseTableFieldData;
 import org.intellij.ibatis.dom.sqlMap.Insert;
 import org.intellij.ibatis.dom.sqlMap.TypeAlias;
+import org.intellij.ibatis.dom.sqlMap.ParameterMap;
 import org.intellij.ibatis.provider.IbatisClassShortcutsReferenceProvider;
 import org.intellij.ibatis.provider.TableColumnReferenceProvider;
-import static org.intellij.ibatis.provider.TableColumnReferenceProvider.getDatabaseTableData;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class GenerateSQLForInsertAction extends PsiIntentionBase {
-	protected void invoke(Project project, Editor editor, PsiFile file, @NotNull PsiElement element) {
+	protected void invoke(Project project, Editor editor, PsiFile file, @NotNull PsiElement insertElement) {
 		if (isAvailable(project, editor, file)) {
+
+			// this will be the parameterClass or parameterMap attribute
 			XmlAttribute attribute;
 
-			// element is the insert tag
-
-			// ok, see if it's a parameter map
-			attribute = ((XmlTag) element).getAttribute("parameterMap");
+			// see if it's got a parameter map
+			attribute = ((XmlTag) insertElement).getAttribute("parameterMap");
 			if (null != attribute) {
 				XmlAttributeValue xmlAttributeValue = attribute.getValueElement();
 				if (null != xmlAttributeValue) {
@@ -39,15 +39,23 @@ public class GenerateSQLForInsertAction extends PsiIntentionBase {
 					if (null != psiReference) {
 						PsiElement psiElement = psiReference.resolve();
 						if (psiElement != null && psiElement instanceof XmlAttribute) {
-
+							XmlTag paramMapTag = ((XmlAttribute) psiElement).getParent();
+							DomElement paramMapTemp = DomManager.getDomManager(project).getDomElement(paramMapTag);
+							if(paramMapTemp != null && paramMapTemp instanceof ParameterMap){
+								ParameterMap pm = (ParameterMap) paramMapTemp;
+								// todo: handle this
+							}
 						}
 					}
 				}
 			}
 
-			// ok, then see if it's a parameter class
-			attribute = ((XmlTag) element).getAttribute("parameterClass");
+			// see if it's a parameter class
+			attribute = ((XmlTag) insertElement).getAttribute("parameterClass");
 			if (null != attribute) {
+				// it has a parameter class
+
+				// this is the value of the attribute
 				XmlAttributeValue xmlAttributeValue = attribute.getValueElement();
 				if (null != xmlAttributeValue) {
 					PsiReference psiReference = xmlAttributeValue.getReference();
@@ -60,45 +68,51 @@ public class GenerateSQLForInsertAction extends PsiIntentionBase {
 							TypeAlias ta = (TypeAlias) typeAliasTemp;
 							PsiClass value = ta.getType().getValue();
 							if(null!=value){
-								String className = value.getQualifiedName();
-								DatabaseTableData tableData = getDatabaseTableData(value);
-								if(null != tableData){
-									List<DatabaseTableFieldData> fieldList = tableData.getFields();
-
-									PsiClass psiClass = IbatisClassShortcutsReferenceProvider.getPsiClass(psiElement, className);
-									// ok, now we have the table meta-data and the class meta-data.
-									// now we can build our insert statement
-									StringBuilder insertStatement = new StringBuilder("\ninsert into ").append(tableData.getName());
-									StringBuilder insertList = new StringBuilder("");
-									StringBuilder valueList = new StringBuilder("");
-									for (DatabaseTableFieldData d : fieldList) {
-										String propName = TableColumnReferenceProvider.getPropNameForColumn(psiClass, d);
-										if(null != propName){
-											if(insertList.length() == 0) {
-												insertList.append(" (");
-											}else{
-												insertList.append(", ");
-											}
-											if(valueList.length() == 0) {
-												valueList.append("\nvalues (");
-											}else{
-												valueList.append(", ");
-											}
-											insertList.append(d.getName());
-											valueList.append("#").append(propName).append("#");
-										}
-									}
-									if(insertList.length() > 0){
-										// ok, build the SQL statement...
-										XmlTag xmlTag = (XmlTag) element;
-										xmlTag.getValue().setText(insertStatement.append(insertList).append(") ").append(valueList).append(")").toString());
-									}
-								}
+								createSqlStatement(insertElement, psiElement, value);
 							}
 						}
+					}else{
+						// is this a class?
+						xmlAttributeValue.getClass();
 					}
 				}
 				
+			}
+		}
+	}
+
+	private void createSqlStatement(PsiElement element, PsiElement psiElement, PsiClass value) {
+		String className = value.getQualifiedName();
+		DatabaseTableData tableData = TableColumnReferenceProvider.getDatabaseTableData(value);
+		if(null != tableData){
+			List<DatabaseTableFieldData> fieldList = tableData.getFields();
+			PsiClass psiClass = IbatisClassShortcutsReferenceProvider.getPsiClass(psiElement, className);
+			// ok, now we have the table meta-data and the class meta-data.
+			// now we can build our insert statement
+			StringBuilder insertStatement = new StringBuilder("\ninsert into ").append(tableData.getName());
+			StringBuilder insertList = new StringBuilder("");
+			StringBuilder valueList = new StringBuilder("");
+			for (DatabaseTableFieldData d : fieldList) {
+				String propName = TableColumnReferenceProvider.getPropNameForColumn(psiClass, d);
+				if(null != propName){
+					if(insertList.length() == 0) {
+						insertList.append(" (");
+					}else{
+						insertList.append(", ");
+					}
+					if(valueList.length() == 0) {
+						valueList.append("\nvalues (");
+					}else{
+						valueList.append(", ");
+					}
+					insertList.append(d.getName());
+					valueList.append("#").append(propName).append("#");
+				}
+			}
+			if(insertList.length() > 0){
+				// ok, build the SQL statement...
+				XmlTag xmlTag = (XmlTag) element;
+				xmlTag.getValue().setText(insertStatement.append(insertList).append(") ").append(valueList).append(")").toString());
 			}
 		}
 	}
