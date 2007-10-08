9e437a57e8b9d4a4224a891dc61526e51c7419fe
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-08 09:47:52 +0000

    in line parameter in SQL
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@280 a27988d2-9830-0410-8773-31d6671a21de

---
+++ src/org/intellij/ibatis/model/InlineParameter.java
@@ -0,0 +1,25 @@
+package org.intellij.ibatis.model;
+
+/**
+ * in line parameter in SQL
+ */
+public class InlineParameter {
+    private String name;
+    private String jdbcType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJdbcType() {
+        return jdbcType;
+    }
+
+    public void setJdbcType(String jdbcType) {
+        this.jdbcType = jdbcType;
+    }
+}
