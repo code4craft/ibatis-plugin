b13e829bd40e17b52c8b5602805bb72eb5d931c0
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-08 09:34:01 +0000

    minor update
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@275 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/provider/ParameterJdbcTypeReferenceProvider.java
+++ src/org/intellij/ibatis/provider/ParameterJdbcTypeReferenceProvider.java
@@ -5,11 +5,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlAttributeValue;
+import org.intellij.ibatis.model.JdbcType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.sql.Types;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -20,67 +19,9 @@ import java.util.Set;
  */
 public class ParameterJdbcTypeReferenceProvider extends BaseReferenceProvider {
 
-    private Map<String, Integer> types;
     private PsiField[] jdbcTypes;
     private PsiField oracleType;
     private PsiField ibatisType;
-
-    public ParameterJdbcTypeReferenceProvider() {
-        init();
-    }
-
-    private void init() {
-        types = new HashMap<String, Integer>();
-        types.put("ARRAY", Types.ARRAY);
-        types.put("BIGINT", Types.BIGINT);
-        types.put("BINARY", Types.BINARY);
-        types.put("BIT", Types.BIT);
-        types.put("BLOB", Types.BLOB);
-        types.put("BOOLEAN", Types.BOOLEAN);
-        types.put("CHAR", Types.CHAR);
-        types.put("CLOB", Types.CLOB);
-        types.put("DATALINK", Types.DATALINK);
-        types.put("DATE", Types.DATE);
-        types.put("DECIMAL", Types.DECIMAL);
-        types.put("DISTINCT", Types.DISTINCT);
-        types.put("DOUBLE", Types.DOUBLE);
-        types.put("FLOAT", Types.FLOAT);
-        types.put("INTEGER", Types.INTEGER);
-        types.put("JAVA_OBJECT", Types.JAVA_OBJECT);
-        types.put("LONGVARBINARY", Types.LONGVARBINARY);
-        types.put("LONGVARCHAR", Types.LONGVARCHAR);
-        types.put("NULL", Types.NULL);
-        types.put("NUMERIC", Types.NUMERIC);
-        types.put("OTHER", Types.OTHER);
-        types.put("REAL", Types.REAL);
-        types.put("REF", Types.REF);
-        types.put("SMALLINT", Types.SMALLINT);
-        types.put("STRUCT", Types.STRUCT);
-        types.put("TIME", Types.TIME);
-        types.put("TIMESTAMP", Types.TIMESTAMP);
-        types.put("TINYINT", Types.TINYINT);
-        types.put("VARBINARY", Types.VARBINARY);
-        types.put("VARCHAR", Types.VARCHAR);
-
-        types.put("CH", Types.CHAR);
-        types.put("VC", Types.VARCHAR);
-
-        types.put("DT", Types.DATE);
-        types.put("TM", Types.TIME);
-        types.put("TS", Types.TIMESTAMP);
-
-        types.put("NM", Types.NUMERIC);
-        types.put("II", Types.INTEGER);
-        types.put("BI", Types.BIGINT);
-        types.put("SI", Types.SMALLINT);
-        types.put("TI", Types.TINYINT);
-
-        types.put("DC", Types.DECIMAL);
-        types.put("DB", Types.DOUBLE);
-        types.put("FL", Types.FLOAT);
-
-        types.put("ORACLECURSOR", -10);
-    }
 
     @NotNull
     public PsiReference[] getReferencesByElement(PsiElement psiElement) {
@@ -94,12 +35,12 @@ public class ParameterJdbcTypeReferenceProvider extends BaseReferenceProvider {
                         return true;
                     }
                 }
-
                 return false;
             }
 
             @Nullable
             public PsiElement resolve() {
+                Map<String, Integer> types= JdbcType.TYPES;
                 String type = getCanonicalText();
                 if (type.length() < 3) {
                     Integer val = types.get(type);
@@ -185,7 +126,7 @@ public class ParameterJdbcTypeReferenceProvider extends BaseReferenceProvider {
             }
 
             public Object[] getVariants() {
-                return types.keySet().toArray();
+                return JdbcType.TYPES.keySet().toArray();
             }
         };
         return new PsiReference[]{psiReference};
