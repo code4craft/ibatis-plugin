dd56adf6f963db75b2b5337051dc4e1de6a65b7c
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-08 09:35:54 +0000

    moved
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@277 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/dom/sqlMap/impl/BaseStatementImpl.java
+++ src/org/intellij/ibatis/dom/sqlMap/impl/BaseStatementImpl.java
@@ -1,11 +1,8 @@
 package org.intellij.ibatis.dom.sqlMap.impl;
 
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.xml.*;
-import com.intellij.util.xml.DomManager;
 import org.intellij.ibatis.dom.sqlMap.*;
 import org.intellij.ibatis.util.IbatisUtil;
+import org.intellij.ibatis.model.InlineParameter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
