9ff65a278cc9162ee467d2dccd8e1930c291a84b
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-08 08:19:11 +0000

    parameter element implementation
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@255 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/dom/sqlMap/impl/IncludeImpl.java
+++ src/org/intellij/ibatis/dom/sqlMap/impl/IncludeImpl.java
@@ -4,7 +4,7 @@ import com.intellij.javaee.model.xml.impl.BaseImpl;
 import org.intellij.ibatis.dom.sqlMap.Include;
 
 /**
- * iinclude element implementation
+ * include element implementation
  */
 public abstract class IncludeImpl extends BaseImpl implements Include {
 }
