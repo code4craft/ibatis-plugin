2135ad831a7e1a27280b47769ae9bec1e517fd50
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-08 09:20:44 +0000

    comment updated
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@272 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/IbatisSqlMapFileDescription.java
+++ src/org/intellij/ibatis/IbatisSqlMapFileDescription.java
@@ -6,6 +6,8 @@ import org.intellij.ibatis.dom.sqlMap.impl.*;
 
 /**
  * file description for iBATIS configuration xml file
+ *
+ * @author  Jacky
  */
 public class IbatisSqlMapFileDescription extends DomFileDescription<SqlMap> {
     public IbatisSqlMapFileDescription() {
@@ -13,19 +15,24 @@ public class IbatisSqlMapFileDescription extends DomFileDescription<SqlMap> {
     }
 
     protected void initializeFileDescription() {
+        //meta information related
         registerImplementation(TypeAlias.class, TypeAliasImpl.class);
         registerImplementation(ParameterMap.class, ParameterMapImpl.class);
+        registerImplementation(Parameter.class, ParameterImpl.class);
         registerImplementation(ResultMap.class, ResultMapImpl.class);
         registerImplementation(Result.class, ResultImpl.class);
+        //SQL code related
         registerImplementation(Sql.class, SqlImpl.class);
+        registerImplementation(Include.class, IncludeImpl.class);
+        //statement related 
         registerImplementation(Select.class, SelectImpl.class);
         registerImplementation(Insert.class, InsertImpl.class);
         registerImplementation(Update.class, UpdateImpl.class);
         registerImplementation(Delete.class, DeleteImpl.class);
         registerImplementation(Statement.class, StatementImpl.class);
         registerImplementation(Procedure.class, ProcedureImpl.class);
+        //other
         registerImplementation(CacheModel.class, CacheModelImpl.class);
-        registerImplementation(Parameter.class, ParameterImpl.class);
-        registerImplementation(Include.class, IncludeImpl.class);
+
     }
 }
