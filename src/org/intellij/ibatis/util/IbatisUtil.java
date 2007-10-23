43ea5cc93e93fb4ea16b4346383851382b87528a
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-23 09:36:27 +0000

    add grep method
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@324 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/util/IbatisUtil.java
+++ src/org/intellij/ibatis/util/IbatisUtil.java
@@ -1,19 +1,24 @@
 package org.intellij.ibatis.util;
 
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.xml.*;
 import com.intellij.util.xml.DomManager;
-import org.intellij.ibatis.dom.sqlMap.Sql;
-import org.intellij.ibatis.facet.IbatisFacetConfiguration;
-import org.intellij.ibatis.facet.IbatisFacet;
-import org.jetbrains.annotations.NotNull;
-import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.intellij.ibatis.dom.sqlMap.Sql;
+import org.intellij.ibatis.facet.IbatisFacet;
+import org.intellij.ibatis.facet.IbatisFacetConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * utility class in iBATIS plug-in
@@ -31,6 +36,7 @@ public class IbatisUtil {
 
     /**
      * get the SQL code in xml tag
+     *
      * @param xmlTag xml tag
      * @return SQL in xml tag
      */
@@ -59,37 +65,71 @@ public class IbatisUtil {
         return sql.toString();
     }
 
-	public static IbatisFacetConfiguration getConfig(PsiElement... elements){
-		if(null == elements || elements.length == 0){
-			return null;
-		}
+    /**
+     * tools just like unix's grep
+     *
+     * @param input   input string
+     * @param pattern regex pattern
+     * @return scanned  string list
+     */
+    public static List<String> grep(String input, String pattern) {
+        List<String> matchedResults = new ArrayList<String>();
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(input);
+        while (m.find()) {
+            matchedResults.add(m.group());
+        }
+        return matchedResults;
+    }
 
-		for (PsiElement e : elements) {
-			if (null != e) {
-				Module module = ModuleUtil.findModuleForPsiElement(e);
-				if (null != module) {
-					IbatisFacet facet = IbatisFacet.getInstance(module);
-					if (facet != null) {
-						return facet.getConfiguration();
-					}
-				}
-			}
-		}
-		return null;
-	}
+    /**
+     * get the configuration for iBATIS
+     *
+     * @param elements element list
+     * @return iBATIS configuration
+     */
+    @Nullable public static IbatisFacetConfiguration getConfig(PsiElement... elements) {
+        if (null == elements || elements.length == 0) {
+            return null;
+        }
 
-	public static String evaluateVelocityTemplate(
-		VelocityContext context,
-		String template
-	) throws Exception {
-		StringWriter sw = new StringWriter();
-		getVelocityEngine().evaluate(context, sw, "iBATIS Plugin", template);
-		return sw.toString();
-	}
+        for (PsiElement e : elements) {
+            if (null != e) {
+                Module module = ModuleUtil.findModuleForPsiElement(e);
+                if (null != module) {
+                    IbatisFacet facet = IbatisFacet.getInstance(module);
+                    if (facet != null) {
+                        return facet.getConfiguration();
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
-	public static VelocityEngine getVelocityEngine() throws Exception {
-		VelocityEngine engine = new VelocityEngine();
-		engine.init();
-		return engine;
-	}
+    /**
+     * evaluate velocity template
+     *
+     * @param context  context
+     * @param template template content
+     * @return output
+     * @throws Exception exception
+     */
+    public static String evaluateVelocityTemplate(VelocityContext context, String template) throws Exception {
+        StringWriter sw = new StringWriter();
+        getVelocityEngine().evaluate(context, sw, "iBATIS Plugin", template);
+        return sw.toString();
+    }
+
+    /**
+     * get the velocity engine
+     *
+     * @return engine object
+     * @throws Exception exception
+     */
+    public static VelocityEngine getVelocityEngine() throws Exception {
+        VelocityEngine engine = new VelocityEngine();
+        engine.init();
+        return engine;
+    }
 }
