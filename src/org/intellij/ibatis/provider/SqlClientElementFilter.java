d04d6ae2d5d8767a0a732172a3c3d6fdd47f9f7a
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-23 05:54:09 +0000

    filter rule used pattern match
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@320 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/provider/SqlClientElementFilter.java
+++ src/org/intellij/ibatis/provider/SqlClientElementFilter.java
@@ -7,51 +7,36 @@ import com.intellij.psi.util.InheritanceUtil;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.intellij.ibatis.util.IbatisUtil;
+
 /**
- * sqlclient method call filter
+ * sql map client method call filter
  */
 public class SqlClientElementFilter implements ElementFilter {
-    public static String operationPattern ="\\w*(query|insert|update|delete)\\w*";
-    public static List<String> operationClass = new ArrayList<String>();
-
-    static {
-        operationClass.add("org.springframework.orm.ibatis.SqlMapClientOperations");
-        operationClass.add("com.ibatis.sqlmap.client.SqlMapExecutor");
-        operationClass.add("com.taobao.common.dao.persistence.SqlMapBaseDAO");
-        operationClass.add("com.ibatis.dao.client.template.SqlMapDaoTemplate");
-    }
+    public static String operationPattern = "(query[(For)|(With)]\\w*)|insert|update|delete";
 
     public boolean isAcceptable(Object o, PsiElement psiElement) {
         PsiLiteralExpression literalExpression = (PsiLiteralExpression) psiElement;
         PsiElement parent = literalExpression.getParent().getParent();
         if (parent != null && parent instanceof PsiMethodCallExpression) {
-            final PsiMethodCallExpression callExpression = (PsiMethodCallExpression) parent;
-            final PsiMethod psiMethod = callExpression.resolveMethod();
-            if (psiMethod != null) {
-                //method validation
-                String methodName = psiMethod.getName().toLowerCase();
-                if (!methodName.matches(operationPattern)) {
+            //first parameter validate
+            PsiElement previousElement = literalExpression.getPrevSibling();
+            if (previousElement != null) {
+                String text1 = previousElement.getText();
+                String text2 = "";
+                if (!text1.equals("(") && previousElement.getPrevSibling() != null) {
+                    text2 = previousElement.getPrevSibling().getText();
+                }
+                if (!(text1.equals("(") || (text1.concat(text2).trim().equals("(")))) {
                     return false;
                 }
-                //first parameter validate
-                PsiElement previousElement = literalExpression.getPrevSibling();
-                if (previousElement != null) {
-                    String text1 = previousElement.getText();
-                    String text2 = "";
-                    if (!text1.equals("(") && previousElement.getPrevSibling() != null) {
-                        text2 = previousElement.getPrevSibling().getText();
-                    }
-                    if (!(text1.equals("(") || (text1.concat(text2).trim().equals("(")))) {
-                        return false;
-                    }
-                }
-
-                //class validate
-                final PsiClass psiClass = psiMethod.getContainingClass();
-                for (String operationClazz : operationClass) {
-                    final PsiClass expectedClass = PsiManager.getInstance(psiClass.getProject()).findClass(operationClazz, psiClass.getResolveScope());
-                    if (InheritanceUtil.isInheritorOrSelf(psiClass, expectedClass, true)) return true;
-                }
+            }
+            //method validation
+            final PsiMethodCallExpression callExpression = (PsiMethodCallExpression) parent;
+            String[] path = callExpression.getMethodExpression().getText().split("\\.");
+            String methodName = path[path.length - 1].trim();
+            if (methodName.matches(operationPattern) && IbatisUtil.getConfig(psiElement) != null) {
+                return true;
             }
         }
         return false;
