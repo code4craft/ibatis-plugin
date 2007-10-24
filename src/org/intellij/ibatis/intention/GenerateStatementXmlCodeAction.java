14af749e183bc602888ffafc1ef2f66373861255
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-24 08:33:26 +0000

    validate logic fixed
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@353 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/intention/GenerateStatementXmlCodeAction.java
+++ src/org/intellij/ibatis/intention/GenerateStatementXmlCodeAction.java
@@ -1,10 +1,12 @@
 package org.intellij.ibatis.intention;
 
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
-import org.jetbrains.annotations.NotNull;
 import org.intellij.ibatis.provider.SqlClientElementFilter;
+import org.intellij.ibatis.provider.StatementIdReferenceProvider;
+import org.intellij.ibatis.util.IbatisUtil;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * generate statement xml code according to sql map client invocation when statement id absent
@@ -16,11 +18,24 @@ public class GenerateStatementXmlCodeAction extends PsiIntentionBase {
     private static final String TEXT = "Generate statement xml code when statement id absent";
 
     protected void invoke(Project project, Editor editor, PsiFile file, @NotNull PsiElement element) {
-
+        if (IbatisUtil.getConfig(element) != null)  //iBATIS enabled
+        {
+            PsiLiteralExpression expression = (PsiLiteralExpression) element;
+            PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) element.getParent().getParent();
+        }
     }
 
+    /**
+     * validate the element in caret is right for intention action
+     *
+     * @param project project
+     * @param editor  editor
+     * @param file    psi file,  java file need
+     * @param element element in caret, PsiLiteralExpression need
+     * @return available mark
+     */
     protected boolean isAvailable(Project project, Editor editor, PsiFile file, @NotNull PsiElement element) {
-        if (element instanceof PsiLiteralExpression) {
+        if (file instanceof PsiJavaFile && element instanceof PsiLiteralExpression) {
             PsiLiteralExpression expression = (PsiLiteralExpression) element;
             PsiElement parent = element.getParent().getParent();
             if (parent instanceof PsiMethodCallExpression) {
@@ -29,9 +44,14 @@ public class GenerateStatementXmlCodeAction extends PsiIntentionBase {
                 String[] path = methodExpression.getText().split("\\.");
                 String methodName = path[path.length - 1].trim();
                 if (methodName.matches(SqlClientElementFilter.operationPattern)) {
-                    PsiReference psiReference = expression.getReference();
-                    if (psiReference != null && psiReference.resolve() == null) {
-                        return true;
+                    PsiReference[] references = expression.getReferences();
+                    for (PsiReference reference : references) {
+                        if (reference instanceof StatementIdReferenceProvider.StatementIdReference) {
+                            if (reference.resolve() == null) {  //the target statement resolved failed
+                                return true;
+                            }
+                            break;
+                        }
                     }
                 }
             }
