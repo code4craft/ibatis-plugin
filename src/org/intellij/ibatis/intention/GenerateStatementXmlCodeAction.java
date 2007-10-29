b3ad54f1f45f78528bc266b5428a7c3dd55ee277
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-29 14:02:46 +0000

    collection parameter information
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@361 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/intention/GenerateStatementXmlCodeAction.java
+++ src/org/intellij/ibatis/intention/GenerateStatementXmlCodeAction.java
@@ -1,12 +1,20 @@
 package org.intellij.ibatis.intention;
 
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.intellij.ibatis.IbatisManager;
+import org.intellij.ibatis.IbatisConfigurationModel;
+import org.intellij.ibatis.facet.IbatisFacetConfiguration;
 import org.intellij.ibatis.provider.SqlClientElementFilter;
 import org.intellij.ibatis.provider.StatementIdReferenceProvider;
 import org.intellij.ibatis.util.IbatisUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 /**
  * generate statement xml code according to sql map client invocation when statement id absent
@@ -18,14 +26,45 @@ public class GenerateStatementXmlCodeAction extends PsiIntentionBase {
     private static final String TEXT = "Generate statement xml code when statement id absent";
 
     protected void invoke(Project project, Editor editor, PsiFile file, @NotNull PsiElement element) {
-        if (IbatisUtil.getConfig(element) != null)  //iBATIS enabled
+        IbatisFacetConfiguration config = IbatisUtil.getConfig(element);
+        if (config != null)  //iBATIS enabled
         {
+            String resultClass = null;
+            String parameterClass = null;
+            String statementId = null;
+            String operationType = null;
             PsiLiteralExpression expression = (PsiLiteralExpression) element;
-            PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) element.getParent().getParent();
-            //todo collect the information for statement xml code creation
+            PsiMethodCallExpression methodCallExpression = PsiTreeUtil.getParentOfType(element, PsiMethodCallExpression.class);
+            PsiReferenceExpression methodExpression = methodCallExpression.getMethodExpression();
+            String[] path = methodExpression.getText().split("\\.");
+            operationType = path[path.length - 1].trim();
+            statementId = expression.getText().replaceAll("\"", "");
+            PsiReturnStatement returnStatement = PsiTreeUtil.getParentOfType(methodExpression, PsiReturnStatement.class);
+            PsiType returnType = returnStatement.getReturnValue().getType();
+            if (returnType != null) {
+                resultClass = returnType.getCanonicalText();
+                if (resultClass.contains("<")) {
+                    resultClass = resultClass.substring(resultClass.indexOf("<") + 1, resultClass.indexOf(">"));
+                }
+            }
+            PsiExpression[] argumentExpressionList = methodCallExpression.getArgumentList().getExpressions();
+            if (argumentExpressionList.length > 1) {
+                parameterClass = argumentExpressionList[1].getType().getCanonicalText();
+            }
+            IbatisConfigurationModel model = IbatisManager.getInstance().getConfigurationModel(ModuleUtil.findModuleForPsiElement(element));
+            Set<XmlFile> sqlMapFiles = model.getSqlMapFiles();
+            for (XmlFile sqlMapFile : sqlMapFiles) {
+                String fileName = sqlMapFile.getVirtualFile().getName();
+            }
+            boolean isSpaceUsed = model.isUseStatementNamespaces();
+            getGenerateCode(null,operationType, statementId, isSpaceUsed, resultClass, parameterClass);
         }
     }
 
+    private void getGenerateCode(PsiFile sqlMapFile,String operationType, String statementId, boolean isSpaceUsed, String resultClass, String parameterClass)
+    {
+     
+    }
     /**
      * validate the element in caret is right for intention action
      *
@@ -43,7 +82,7 @@ public class GenerateStatementXmlCodeAction extends PsiIntentionBase {
                 //method name validation simply
                 PsiReferenceExpression methodExpression = ((PsiMethodCallExpression) parent).getMethodExpression();
                 String[] path = methodExpression.getText().split("\\.");
-                String methodName = path[path.length - 1].trim();
+                String methodName = path[path.length - 1].trim().toLowerCase();
                 if (methodName.matches(SqlClientElementFilter.operationPattern)) {
                     PsiReference[] references = expression.getReferences();
                     for (PsiReference reference : references) {
