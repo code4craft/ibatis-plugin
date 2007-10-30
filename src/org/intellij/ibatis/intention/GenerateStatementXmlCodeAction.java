52f1f80610da8bbed284f6a240608988eef7579a
libing.chen@gmail.com <libing.chen@gmail.com@a27988d2-9830-0410-8773-31d6671a21de>
2007-10-30 10:52:17 +0000

    generation logic implemented
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@365 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/intention/GenerateStatementXmlCodeAction.java
+++ src/org/intellij/ibatis/intention/GenerateStatementXmlCodeAction.java
@@ -1,20 +1,26 @@
 package org.intellij.ibatis.intention;
 
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
-import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.util.PsiTreeUtil;
-import org.intellij.ibatis.IbatisManager;
+import com.intellij.psi.xml.*;
+import com.intellij.util.xml.DomFileElement;
+import com.intellij.util.xml.DomManager;
 import org.intellij.ibatis.IbatisConfigurationModel;
+import org.intellij.ibatis.IbatisManager;
+import org.intellij.ibatis.dom.sqlMap.*;
 import org.intellij.ibatis.facet.IbatisFacetConfiguration;
-import org.intellij.ibatis.provider.SqlClientElementFilter;
-import org.intellij.ibatis.provider.StatementIdReferenceProvider;
+import org.intellij.ibatis.provider.*;
 import org.intellij.ibatis.util.IbatisUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * generate statement xml code according to sql map client invocation when statement id absent
@@ -35,36 +41,181 @@ public class GenerateStatementXmlCodeAction extends PsiIntentionBase {
             String operationType = null;
             PsiLiteralExpression expression = (PsiLiteralExpression) element;
             PsiMethodCallExpression methodCallExpression = PsiTreeUtil.getParentOfType(element, PsiMethodCallExpression.class);
+            if (methodCallExpression == null) return;
             PsiReferenceExpression methodExpression = methodCallExpression.getMethodExpression();
             String[] path = methodExpression.getText().split("\\.");
             operationType = path[path.length - 1].trim();
             statementId = expression.getText().replaceAll("\"", "");
             PsiReturnStatement returnStatement = PsiTreeUtil.getParentOfType(methodExpression, PsiReturnStatement.class);
-            PsiType returnType = returnStatement.getReturnValue().getType();
-            if (returnType != null) {
-                resultClass = returnType.getCanonicalText();
-                if (resultClass.contains("<")) {
-                    resultClass = resultClass.substring(resultClass.indexOf("<") + 1, resultClass.indexOf(">"));
+            if (returnStatement != null) {
+                PsiExpression returnValue = returnStatement.getReturnValue();
+                if (returnValue != null) {
+                    PsiType returnType = returnValue.getType();
+                    if (returnType != null) {
+                        resultClass = returnType.getCanonicalText();
+                        if (resultClass.contains("<")) {  //generic in java 5
+                            resultClass = resultClass.substring(resultClass.indexOf("<") + 1, resultClass.indexOf(">"));
+                        } else  //common class name
+                        {
+                            //ignore collection class
+                            if (resultClass.equals("java.util.List") || resultClass.equals("java.util.Map")) {
+                                resultClass = null;
+                            }
+                        }
+                    }
                 }
             }
             PsiExpression[] argumentExpressionList = methodCallExpression.getArgumentList().getExpressions();
             if (argumentExpressionList.length > 1) {
                 parameterClass = argumentExpressionList[1].getType().getCanonicalText();
+                if(parameterClass.equals("null")) parameterClass=null;
             }
             IbatisConfigurationModel model = IbatisManager.getInstance().getConfigurationModel(ModuleUtil.findModuleForPsiElement(element));
-            Set<XmlFile> sqlMapFiles = model.getSqlMapFiles();
-            for (XmlFile sqlMapFile : sqlMapFiles) {
-                String fileName = sqlMapFile.getVirtualFile().getName();
+            if (model != null) {
+                boolean isSpaceUsed = model.isUseStatementNamespaces();
+                XmlFile destinationSQLMapFile = null;  //if destination file not found, the last one will be used
+                Set<XmlFile> sqlMapFiles = model.getSqlMapFiles();
+                for (XmlFile sqlMapFile : sqlMapFiles) {
+                    String fileName = sqlMapFile.getName().trim();
+                    XmlDocument document = sqlMapFile.getDocument();
+                    if (document != null) {
+                        XmlTag rootTag = document.getRootTag();
+                        if (rootTag != null) {
+                            destinationSQLMapFile = sqlMapFile;
+                            String nameSpace = rootTag.getAttributeValue("namespace");
+                            if (isMatchedSQLMapFile(fileName, nameSpace, statementId)) {
+                                break;
+                            }
+                        }
+                    }
+                }
+                //destination found?
+                if (destinationSQLMapFile != null) {
+                    XmlTag xmlTag = generateStatementXMLCode(destinationSQLMapFile, operationType, statementId, isSpaceUsed, resultClass, parameterClass);
+                    //open the SQL Map file and navigate to the xml tag
+                    if (xmlTag != null) {
+                        OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(xmlTag.getAttribute("id").getValueElement());
+                        FileEditorManager.getInstance(project).openEditor(fileDescriptor, true);
+                    }
+                }
             }
-            boolean isSpaceUsed = model.isUseStatementNamespaces();
-            getGenerateCode(null,operationType, statementId, isSpaceUsed, resultClass, parameterClass);
         }
     }
 
-    private void getGenerateCode(PsiFile sqlMapFile,String operationType, String statementId, boolean isSpaceUsed, String resultClass, String parameterClass)
-    {
-     
+    /**
+     * validate file name is suitable for writing xml code
+     *
+     * @param fileName    file name
+     * @param nameSpace   name space
+     * @param statementId statement id
+     * @return match mark
+     */
+    public boolean isMatchedSQLMapFile(@NotNull String fileName, @Nullable String nameSpace, @NotNull String statementId) {
+        String[] parts = statementId.split("\\.");
+        fileName = fileName.toLowerCase().replace("\\.xml", ""); // file name converter
+        //name space and file name match
+        boolean isMatched = fileName.contains(parts[0]) || parts[0].equals(nameSpace);
+        //words match, "." not included
+        if (!isMatched && !statementId.contains(".")) {
+            List<String> words = IbatisUtil.grep(parts[parts.length - 1], "[A-Z]*[a-z]*");
+            for (String word : words) {
+                if (word.length() > 3 && fileName.contains(word.toLowerCase())) {
+                    isMatched = true;
+                    break;
+                }
+            }
+        }
+        return isMatched;
     }
+
+    /**
+     * generate statement xml code
+     *
+     * @param sqlMapFile     destination file
+     * @param operationType  operation type
+     * @param statementId    statement id
+     * @param isSpaceUsed    name space used?
+     * @param resultClass    result class
+     * @param parameterClass parameter class
+     * @return generated xml tag
+     */
+    @Nullable
+    private XmlTag generateStatementXMLCode(XmlFile sqlMapFile, String operationType, String statementId, boolean isSpaceUsed, String resultClass, String parameterClass) {
+        DomFileElement<SqlMap> fileElement = DomManager.getDomManager(sqlMapFile.getProject()).getFileElement(sqlMapFile, SqlMap.class);
+        if (fileElement == null) return null;
+        SqlMap sqlMap = fileElement.getRootElement();
+        BaseStatement statement = createStatement(operationType, sqlMap);
+        statement.getId().setStringValue(isSpaceUsed ? statementId.split("\\.")[0] : statementId);
+        //parameterClass assign
+        if (StringUtil.isNotEmpty(parameterClass)) {
+            boolean isShortCut = false;  // indicate shortcut
+            //internal type alias introduced
+            for (Map.Entry<String, String> entry : IbatisClassShortcutsReferenceProvider.classShortcuts.entrySet()) {
+                if (entry.getValue().equals(parameterClass)) {
+                    parameterClass = entry.getKey();
+                    isShortCut = true;
+                    break;
+                }
+            }
+            if (!isShortCut) {   // none shortcut class
+                //type alias introduced
+                Map<String, PsiClass> typeAliasMap = IbatisManager.getInstance().getAllTypeAlias(sqlMapFile);
+                for (Map.Entry<String, PsiClass> entry : typeAliasMap.entrySet()) {
+                    if (parameterClass.equals(entry.getValue().getQualifiedName())) {
+                        parameterClass = entry.getKey();
+                        break;
+                    }
+                }
+            }
+            statement.getParameterClass().setStringValue(parameterClass);
+        }
+        //resultMap or resultClass assign
+        if (StringUtil.isNotEmpty(resultClass) && statement instanceof BaseResultStatement) {
+            BaseResultStatement resultStatement = (BaseResultStatement) statement;
+            String resultMapId = null;
+            Map<String, PsiClass> resultMap = IbatisManager.getInstance().getAllResultMap(sqlMapFile);
+            for (Map.Entry<String, PsiClass> entry : resultMap.entrySet()) {
+                if (resultClass.equals(entry.getValue().getQualifiedName())) {
+                    resultMapId = entry.getKey();
+                }
+            }
+            if (StringUtil.isNotEmpty(resultMapId)) {
+                resultStatement.getResultMap().setStringValue(resultMapId);
+            } else if (StringUtil.isNotEmpty(resultClass)) {
+                resultStatement.getResultClass().setStringValue(resultClass);
+            }
+        }
+        //default content
+        XmlTag tag = statement.getXmlTag();
+        if (tag != null) {
+            tag.getValue().setText("\n");
+        }
+        return tag;
+    }
+
+    /**
+     * create statement according to operation type
+     *
+     * @param operationType operation type, such as insert, queryForObject
+     * @param sqlMap        SQLMap object
+     * @return BaseStatement or sub object
+     */
+    private BaseStatement createStatement(String operationType, SqlMap sqlMap) {
+        BaseStatement statement;
+        if (operationType.contains("insert")) {
+            statement = sqlMap.addInsert();
+        } else if (operationType.contains("delete")) {
+            statement = sqlMap.addDelete();
+        } else if (operationType.contains("update")) {
+            statement = sqlMap.addUpdate();
+        } else if (operationType.contains("query")) {
+            statement = sqlMap.addSelect();
+        } else {
+            statement = sqlMap.addStatement();
+        }
+        return statement;
+    }
+
     /**
      * validate the element in caret is right for intention action
      *
