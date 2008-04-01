74ed3123d36aaf410969e3b6c18bc77735c1b8b8
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2008-04-01 03:23:57 +0000

    minor update
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@476 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/inspections/ResultMapInSelectInspection.java
+++ src/org/intellij/ibatis/inspections/ResultMapInSelectInspection.java
@@ -66,7 +66,7 @@ public class ResultMapInSelectInspection extends SqlMapInspection {
                         }
                     }
                 } catch (ParseException e) {
-                         e.printStackTrace();
+                        
                 }
             }
         }
