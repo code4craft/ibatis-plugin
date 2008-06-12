a2c6baccc4aa226c8bac848fb7c338e191fb8d44
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2008-06-12 13:10:32 +0000

    use new PsiReferenceProvider
    git-svn-id: http://ibatis-plugin.googlecode.com/svn/trunk@486 a27988d2-9830-0410-8773-31d6671a21de

--- src/org/intellij/ibatis/provider/BaseReferenceProvider.java
+++ src/org/intellij/ibatis/provider/BaseReferenceProvider.java
@@ -2,31 +2,22 @@ package org.intellij.ibatis.provider;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.impl.source.resolve.reference.PsiReferenceProvider;
-import com.intellij.psi.impl.source.resolve.reference.ReferenceType;
-import com.intellij.psi.scope.PsiScopeProcessor;
+import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * base reference
+ * base reference  provider
  */
-public class BaseReferenceProvider implements PsiReferenceProvider {
-    @NotNull public PsiReference[] getReferencesByElement(PsiElement psiElement) {
-        return PsiReference.EMPTY_ARRAY;
-    }
-
-    @Deprecated
-    @NotNull public PsiReference[] getReferencesByElement(PsiElement psiElement, ReferenceType referenceType) {
-        return new PsiReference[0];
+public class BaseReferenceProvider extends PsiReferenceProvider {
+    @NotNull
+    public PsiReference[] getReferencesByElement(@NotNull PsiElement psiElement, @NotNull ProcessingContext processingContext) {
+        return getReferencesByElement(psiElement);
     }
 
     @NotNull
-    @Deprecated
-    public PsiReference[] getReferencesByString(String s, PsiElement psiElement, ReferenceType referenceType, int i) {
-        return new PsiReference[0];  
+    public PsiReference[] getReferencesByElement(PsiElement psiElement) {
+        return PsiReference.EMPTY_ARRAY;
     }
 
-    public void handleEmptyContext(PsiScopeProcessor psiScopeProcessor, PsiElement psiElement) {
-
-    }
 }
